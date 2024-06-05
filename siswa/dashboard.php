409a3b1c1ba885bd69388dda6ca144099412fc7a
Mazemad <emannuelhenji521@gmail.com>
2024-06-05 23:47:52 +0700

    modified: siswa/dashboard.php

--- siswa/dashboard.php
+++ siswa/dashboard.php
@@ -1,4 +1,10 @@
-<style>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Aspirasiku</title>
+    <!-- Include Materialize CSS -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css" rel="stylesheet">
+	<style>
 	/* Aturan CSS responsif */
 @media screen and (max-width: 600px) {
     table.responsive-table {
@@ -59,186 +65,202 @@
 }
 
 </style>
+</head>
+<body>
 
-<table class="responsive-table" border="2" style="width: 100%;">
-	<tr>
-		<td><h4 class="gray-text hide-on-med-and-down">Tulis Aspirasi</h4></td>
-		<td><h4 class="gray-text hide-on-med-and-down">Daftar Aspirasi</h4></td>
-	</tr>
-	<tr>
-		<td style="padding: 20px;">
-			<form method="POST" enctype="multipart/form-data">
-				<textarea class="materialize-textarea" name="laporan" placeholder="Tulis Aspirasi"></textarea><br><br>
-				<label>Kategori</label>
-      			  <select name="kategori" class="browser-default">
-         		   <option value="Kurikulum">Kurikulum</option>
-         		   <option value="Kesiswaan">Kesiswaan</option>
-         		   <option value="Sarana dan Prasarana">Sarana dan Prasarana</option>
-          		  <option value="Hubungan Masyarakat">Hubungan Masyarakat</option>
-       			 </select><br><br>
-				<label>Gambar</label>
-				<input type="file" name="foto"><br><br>
-				<input type="submit" name="kirim" value="Kirim" class="btn blue">
-			</form>
-		</td>
+<div class="container">
+    <table class="responsive-table" border="2" style="width: 100%;">
+        <tr>
+            <td><h4 class="gray-text hide-on-med-and-down">Tulis Aspirasi</h4></td>
+            <td><h4 class="gray-text hide-on-med-and-down">Daftar Aspirasi</h4></td>
+        </tr>
+        <tr>
+            <td style="padding: 20px;">
+                <form method="POST" enctype="multipart/form-data">
+                    <textarea class="materialize-textarea" name="laporan" placeholder="Tulis Aspirasi"></textarea><br><br>
+                    <label>Kategori</label>
+                    <select name="kategori" class="browser-default">
+                        <option value="Kurikulum">Kurikulum</option>
+                        <option value="Kesiswaan">Kesiswaan</option>
+                        <option value="Sarana dan Prasarana">Sarana dan Prasarana</option>
+                        <option value="Hubungan Masyarakat">Hubungan Masyarakat</option>
+                    </select><br><br>
+                    <label>Gambar</label>
+                    <input type="file" name="foto"><br><br>
+                    <input type="submit" name="kirim" value="Kirim" class="btn blue">
+                </form>
+            </td>
+            <td>
+                <table border="3" class="responsive-table striped highlight">
+                    <thead>
+                        <tr>
+                            <td>No</td>
+                            <td>NIS</td>
+                            <td>Nama</td>
+                            <td>Kategori</td>
+                            <td>Tanggal Masuk</td>
+                            <td>Status</td>
+                            <td>Opsi</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php 
+                        error_reporting(E_ALL);
+                        ini_set('display_errors', 1);
 
-		<td>
+                        $no = 1;
+                        $query = "
+                            SELECT 
+                                a.id_aspirasi, 
+                                a.tgl_aspirasi, 
+                                a.nis, 
+                                a.kategori, 
+                                a.isi_laporan, 
+                                a.foto, 
+                                a.status, 
+                                s.nama, 
+                                s.username, 
+                                s.password, 
+                                s.email, 
+                                t.id_tanggapan, 
+                                t.tgl_tanggapan, 
+                                t.tanggapan, 
+                                t.id_petugas 
+                            FROM 
+                                aspirasi a 
+                            INNER JOIN 
+                                siswa s ON a.nis = s.nis 
+                            LEFT JOIN 
+                                tanggapan t ON a.id_aspirasi = t.id_aspirasi 
+                            WHERE 
+                                a.nis = '" . $_SESSION['data']['nis'] . "' 
+                                AND (a.status = 'selesai' OR t.id_aspirasi IS NULL) 
+                            ORDER BY 
+                                a.id_aspirasi DESC
+                        ";
+                        $aspirasi = mysqli_query($koneksi, $query);
 
-			<table border="3" class="responsive-table striped highlight">
-				<tr>
-					<td>No</td>
-					<td>NIS</td>
-					<td>Nama</td>
-					<td>Kategori</td>
-					<td>Tanggal Masuk</td>
-					<td>Status</td>
-					<td>Opsi</td>
-				</tr>
-	
-				<?php 
-				error_reporting(E_ALL);
-				ini_set('display_errors', 1);
-					$no = 1;
-					$aspirasi = mysqli_query($koneksi, "
-						SELECT 
-							a.id_aspirasi, 
-							a.tgl_aspirasi, 
-							a.nis, 
-							a.kategori, 
-							a.isi_laporan, 
-							a.foto, 
-							a.status, 
-							s.nama, 
-							s.username, 
-							s.password, 
-							s.email, 
-							t.id_tanggapan, 
-							t.tgl_tanggapan, 
-							t.tanggapan, 
-							t.id_petugas 
-						FROM 
-							aspirasi a 
-						INNER JOIN 
-							siswa s ON a.nis = s.nis 
-						LEFT JOIN 
-							tanggapan t ON a.id_aspirasi = t.id_aspirasi 
-						WHERE 
-							a.nis = '".$_SESSION['data']['nis']."' 
-							AND (a.status = 'elesai' OR t.id_aspirasi IS NULL) 
-						ORDER BY 
-							a.id_aspirasi DESC
-					");
-					while ($r = mysqli_fetch_assoc($aspirasi)) { 				
-						<tr>
-							<td><?php echo $no++; ?></td>
-							<td><?php echo $r['nis']; ?></td>
-							<td><?php echo $r['nama']; ?></td>
-							<td><?php echo $r['kategori']; ?></td>
-							<td><?php echo $r['tgl_aspirasi']; ?></td>
-							<td><?php echo $r['status']; ?></td>
-							<td>
-								<a class="btn modal-trigger blue" href="#tanggapan&id_aspirasi=<?php echo $r['id_aspirasi'] ?>">More</a>
-								<a class="btn red" onclick="return confirm('Anda Yakin Ingin Menghapus Y/N')" 
-									href="index.php?p=aspirasi_hapus&id_aspirasi=<?php echo $r['id_aspirasi'] ?>">Hapus</a> 
-							</td>
-				<!-- ------------------------------------------------------------------------------------------------------------------------------------ -->
-						<!-- Modal Structure -->
-						<div id="tanggapan&id_aspirasi=<?php echo $r['id_aspirasi'] ?>" class="modal">
-						<div class="modal-content">
-							<h4 class="gray-text">Detail</h4>
-							<div class="col s12 m6">
-								<p>NIS : <?php echo $r['nis']; ?></p>
-								<p>Dari : <?php echo $r['nama']; ?></p>
-								<p>Petugas : <?php echo isset($r['nama_petugas']) ? $r['nama_petugas'] : 'Belum ditanggapi'; ?></p>
-								<p>Kategori : <?php echo $r['kategori']; ?></p>
-								<p>Tanggal Masuk : <?php echo $r['tgl_aspirasi']; ?></p>
-								<p>Tanggal Ditanggapi : <?php echo $r['tgl_tanggapan']; ?></p>
-								<?php 
-									if($r['foto']=="kosong"){ ?>
-										<img src="../img/noImage.png" width="100">
-								<?php	}else{ ?>
-									<img width="100" src="../img/<?php echo $r['foto']; ?>">
-								<?php }
-								?>
-								<br><b>Pesan</b>
-								<p><?php echo $r['isi_laporan']; ?></p>
-								<br><b>Respon</b>
-								<p><?php echo $r['tanggapan']; ?></p>
-							</div>
-						
-							</div>
-						<div class="modal-footer col s12">
-							<a href="#!" class="modal-close waves-effect waves-green btn-flat">Close</a>
-						</div>
-						</div>
-				<!-- ------------------------------------------------------------------------------------------------------------------------------------ -->
+                        while ($r = mysqli_fetch_assoc($aspirasi)) {
+                        ?>
+                        <tr>
+                            <td><?php echo $no++; ?></td>
+                            <td><?php echo $r['nis']; ?></td>
+                            <td><?php echo $r['nama']; ?></td>
+                            <td><?php echo $r['kategori']; ?></td>
+                            <td><?php echo $r['tgl_aspirasi']; ?></td>
+                            <td><?php echo $r['status']; ?></td>
+                            <td>
+                                <a class="btn modal-trigger blue" href="#tanggapan<?php echo $r['id_aspirasi']; ?>">More</a>
+                                <a class="btn red" onclick="return confirm('Anda Yakin Ingin Menghapus Y/N')" 
+                                   href="index.php?p=aspirasi_hapus&id_aspirasi=<?php echo $r['id_aspirasi']; ?>">Hapus</a> 
+                            </td>
+                        </tr>
+                        <!-- Modal Structure -->
+                        <div id="tanggapan<?php echo $r['id_aspirasi']; ?>" class="modal">
+                            <div class="modal-content">
+                                <h4 class="gray-text">Detail</h4>
+                                <div class="col s12 m6">
+                                    <p>NIS : <?php echo $r['nis']; ?></p>
+                                    <p>Dari : <?php echo $r['nama']; ?></p>
+                                    <p>Petugas : <?php echo isset($r['nama_petugas']) ? $r['nama_petugas'] : 'Belum ditanggapi'; ?></p>
+                                    <p>Kategori : <?php echo $r['kategori']; ?></p>
+                                    <p>Tanggal Masuk : <?php echo $r['tgl_aspirasi']; ?></p>
+                                    <p>Tanggal Ditanggapi : <?php echo $r['tgl_tanggapan']; ?></p>
+                                    <?php 
+                                    if ($r['foto'] == "kosong") { ?>
+                                        <img src="../img/noImage.png" width="100">
+                                    <?php } else { ?>
+                                        <img width="100" src="../img/<?php echo $r['foto']; ?>">
+                                    <?php } ?>
+                                    <br><b>Pesan</b>
+                                    <p><?php echo $r['isi_laporan']; ?></p>
+                                    <br><b>Respon</b>
+                                    <p><?php echo $r['tanggapan']; ?></p>
+                                </div>
+                            </div>
+                            <div class="modal-footer col s12">
+                                <a href="#!" class="modal-close waves-effect waves-green btn-flat">Close</a>
+                            </div>
+                        </div>
+                        <?php } ?>
+                    </tbody>
+                </table>
+            </td>
+        </tr>
+    </table>
+</div>
 
-						</tr>
-							<?php  }
-							?>
+<?php 
+if (isset($_POST['kirim'])) {
+    // Retrieve form data
+    $nis = $_SESSION['data']['nis'];
+    $kategori = $_POST['kategori'];
+    $tgl = date('Y-m-d');
 
-						</tbody>
-						</table>  
-					<?php 
-						if (isset($_POST['kirim'])) {
-							// Retrieve form data
-							$nis = $_SESSION['data']['nis'];
-							$kategori = $_POST['kategori'];
-							$tgl = date('Y-m-d');
-						
-							// File data
-							$foto = $_FILES['foto']['name'];
-							$source = $_FILES['foto']['tmp_name'];
-							$folder = './../img/';
-							$listeks = array('jpg', 'png', 'jpeg', 'img');
-							$eks = strtolower(pathinfo($foto, PATHINFO_EXTENSION));
-							$size = $_FILES['foto']['size'];
-							$nama = date('dmYis') . $foto;
-						
-							// Check if file is uploaded
-							if ($foto != "") {
-								if (in_array($eks, $listeks)) {
-									if ($size <= 10000000) {
-										if (move_uploaded_file($source, $folder . $nama)) {
-											// Escaping the laporan input to prevent SQL injection
-											$laporan = mysqli_real_escape_string($koneksi, $_POST['laporan']);
-											
-											// Inserting data into the database
-											$query = "INSERT INTO aspirasi (tgl_aspirasi, nis, kategori, isi_laporan, foto, status) VALUES ('$tgl', '$nis', '$kategori', '$laporan', '$nama', 'proses')";
-											$result = mysqli_query($koneksi, $query);
-						
-											if ($result) {
-												echo "<script>alert('Aspirasi akan segera diproses')</script>";
-												echo "<script>location='index.php';</script>";
-											} else {
-												echo "<script>alert('Gagal menyimpan aspirasi ke database')</script>";
-												echo "Error: " . mysqli_error($koneksi);
-											}
-										} else {
-											echo "<script>alert('Gagal mengunggah file')</script>";
-										}
-									} else {
-										echo "<script>alert('Ukuran gambar tidak boleh lebih dari 10MB')</script>";
-									}
-								} else {
-									echo "<script>alert('Format file tidak didukung')</script>";
-								}
-							} else {
-								// Handle case when no file is uploaded
-								$laporan = mysqli_real_escape_string($koneksi, $_POST['laporan']);
-								
-								// Inserting data into the database without file
-								$query = "INSERT INTO aspirasi (tgl_aspirasi, nis, kategori, isi_laporan, status) VALUES ('$tgl', '$nis', '$kategori', '$laporan', 'proses')";
-								$result = mysqli_query($koneksi, $query);
-						
-								if ($result) {
-									echo "<script>alert('Aspirasi akan segera diproses')</script>";
-									echo "<script>location='index.php';</script>";
-								} else {
-									echo "<script>alert('Gagal menyimpan aspirasi ke database')</script>";
-									echo "Error: " . mysqli_error($koneksi);
-								}
-							}
-						}						
+    // File data
+    $foto = $_FILES['foto']['name'];
+    $source = $_FILES['foto']['tmp_name'];
+    $folder = './../img/';
+    $listeks = array('jpg', 'png', 'jpeg', 'img');
+    $eks = strtolower(pathinfo($foto, PATHINFO_EXTENSION));
+    $size = $_FILES['foto']['size'];
+    $nama = date('dmYis') . $foto;
 
-					?>
+    // Check if file is uploaded
+    if ($foto != "") {
+        if (in_array($eks, $listeks)) {
+            if ($size <= 10000000) {
+                if (move_uploaded_file($source, $folder . $nama)) {
+                    // Escaping the laporan input to prevent SQL injection
+                    $laporan = mysqli_real_escape_string($koneksi, $_POST['laporan']);
+                    
+                    // Inserting data into the database
+                    $query = "INSERT INTO aspirasi (tgl_aspirasi, nis, kategori, isi_laporan, foto, status) VALUES ('$tgl', '$nis', '$kategori', '$laporan', '$nama', 'proses')";
+                    $result = mysqli_query($koneksi, $query);
+
+                    if ($result) {
+                        echo "<script>alert('Aspirasi akan segera diproses')</script>";
+                        echo "<script>location='index.php';</script>";
+                    } else {
+                        echo "<script>alert('Gagal menyimpan aspirasi ke database')</script>";
+                        echo "Error: " . mysqli_error($koneksi);
+                    }
+                } else {
+                    echo "<script>alert('Gagal mengunggah file')</script>";
+                }
+            } else {
+                echo "<script>alert('Ukuran gambar tidak boleh lebih dari 10MB')</script>";
+            }
+        } else {
+            echo "<script>alert('Format file tidak didukung')</script>";
+        }
+    } else {
+        // Handle case when no file is uploaded
+        $laporan = mysqli_real_escape_string($koneksi, $_POST['laporan']);
+        
+        // Inserting data into the database without file
+        $query = "INSERT INTO aspirasi (tgl_aspirasi, nis, kategori, isi_laporan, status) VALUES ('$tgl', '$nis', '$kategori', '$laporan', 'proses')";
+        $result = mysqli_query($koneksi, $query);
+
+        if ($result) {
+            echo "<script>alert('Aspirasi akan segera diproses')</script>";
+            echo "<script>location='index.php';</script>";
+        } else {
+            echo "<script>alert('Gagal menyimpan aspirasi ke database')</script>";
+            echo "Error: " . mysqli_error($koneksi);
+        }
+    }
+}
+?>
+
+<!-- Include jQuery and Materialize JS -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+<script>
+    $(document).ready(function(){
+        $('.modal').modal();
+    });
+</script>
+
+</body>
+</html>
